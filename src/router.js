7c311f50eef39a37362982141fab170741dd9108
Joe Connolly <jcjconnolly4@gmail.com>
2019-05-16 22:19:27 -0400

    testing

--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ import * as Products from './controllers/product_controller';
 const router = Router();
 
 router.get('/', (req, res) => {
-  res.json({ message: 'welcome to our query products api!' });
+  res.json({ message: 'welcome to our query products 2!' });
 });
 
 // /your routes will go here
